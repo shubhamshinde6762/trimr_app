ffa58b3897e029259c0f2d8e965a51a708a905b7
Shubham Shinde <shubhamshinde6762@gmail.com>
2025-12-12 22:13:33 +0530

    next setup added

---
+++ src/components/Home/HomeFeatures.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeFeatures = () => {
+  return (
+    <div>HomeFeatures</div>
+  )
+}
+
+export default HomeFeatures
---
+++ src/components/Shortener/ShortenerBox.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShortenerBox = () => {
+  return (
+    <div>ShortenerBox</div>
+  )
+}
+
+export default ShortenerBox
